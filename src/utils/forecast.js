6d8960515257cc9ce65bd3f4d362b1f49a7358f6
Pritesh Samantarai <ksamantarai@gmail.com>
2021-10-23 19:36:14 +0530

    Added Humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degress out. But feels like ' + body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degress out. But feels like ' + body.current.feelslike + '. Humidity is '+body.current.humidity+'%.')
         }
     })
 }
